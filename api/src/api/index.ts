a83f932939d5be8657faedfad80bb6b25778d07a
Kamen Kanev <kamen.e.kanev@gmail.com>
2022-04-30 17:16:26 +0300

    Restructure the repo

--- api/src/api/index.ts
+++
@@ -1,4 +0,0 @@
-import * as serverless from 'serverless-http';
-import api from './api';
-
-export const handler = serverless(api);
--- generate_bg_captions.py
+++
@@ -1,6 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-video_id = "XPeeCyJReZw"
-transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-transcript = transcript_list.find_generated_transcript(['en'])
-translated_transcript = transcript.translate('bg')
-print(translated_transcript.fetch())
