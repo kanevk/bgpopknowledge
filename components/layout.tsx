cdd0e1c1f37dfa3595f24bfc94a6e19de5037e21
Kamen Kanev <kamen.e.kanev@gmail.com>
2022-05-01 11:39:36 +0300

    Replace favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -23,7 +23,7 @@ export default function Layout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         {pageTitle && <title>{pageTitle}</title>}
-        <link rel="icon" href="/favicon.jpeg" />
+        <link rel="icon" href="/green-7.svg" />
         <meta property="og:site_name" content="На Зелено" key="ogsitename" />
         {pageTitle && (
           <meta property="og:title" content={pageTitle} key="ogtitle" />
