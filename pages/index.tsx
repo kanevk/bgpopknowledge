321b5def61ef2381351f0c9502d20c9ce997c9ff
Kamen Kanev <kamen.e.kanev@gmail.com>
2022-09-28 00:39:28 +0300

    Add translated video generator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,37 @@
-import { Link } from "@mui/material";
+import {
+  Button,
+  Link,
+  Stack,
+  TextField,
+  Tooltip,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [youtubeLink, setYoutubeLink] = useState<string>();
+  const hasYoutubeLinkError =
+    !youtubeLink ||
+    !(youtubeLink.includes("youtube.com") || youtubeLink.includes("youtu.be"));
+
+  const openYoutubeLink = () => {
+    if (!youtubeLink) throw Error("Missing youtube link");
+
+    const youtubeIdWeb = youtubeLink.split("v=")[1]?.split("&")[0];
+    const youtubeLinkMobile = youtubeLink.split("youtu.be/")[1]?.split("?")[0];
+
+    router.push(`/videos/${youtubeIdWeb || youtubeLinkMobile}`);
+  };
+
   return (
     <Layout
       pageTitle="На Зелено"
@@ -13,22 +40,33 @@ const Home: NextPage = () => {
     >
       <div className={styles.container}>
         <main className={styles.main}>
-          <ul>
-            <li>
-              <Link href="/videos/lXfEK8G8CUI">
-                How the immune system actually works
-              </Link>
-            </li>
-            <li>
-              <Link href="/videos/X9GRsZvvvik">
-                Ukraine War: Putin says Mariupol situation is ...
-              </Link>
-            </li>
-          </ul>
+          <Stack sx={{ marginTop: 5 }} alignItems="center" spacing={2}>
+            <TextField
+              label="Въведете YouTube видео линк"
+              sx={{ width: isMobile ? 300 : 600 }}
+              value={youtubeLink}
+              onChange={(e) => setYoutubeLink(e.currentTarget.value)}
+            />
+            <Tooltip
+              title={
+                hasYoutubeLinkError ? "Моля поставете валиден YouTube линк" : ""
+              }
+            >
+              <span>
+                <Button
+                  disabled={hasYoutubeLinkError}
+                  variant="contained"
+                  sx={{ width: 200 }}
+                  onClick={openYoutubeLink}
+                >
+                  Гледай с превод
+                </Button>
+              </span>
+            </Tooltip>
+          </Stack>
         </main>
 
-        <footer className={styles.footer}>
-        </footer>
+        <footer className={styles.footer}></footer>
       </div>
     </Layout>
   );
