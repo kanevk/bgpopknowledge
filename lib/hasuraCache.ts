f6abc976d4df8f79a9088e328d41d3f79793956a
Kamen Kanev <kamen.e.kanev@gmail.com>
2023-04-05 21:28:00 +0300

    Move to Hasura over Nhost

--- lib/hasuraCache.ts
+++ lib/hasuraCache.ts
@@ -1,11 +1,31 @@
-import { NhostClient } from "@nhost/nhost-js";
+const ADMIN_SECRET =
+  "9LFbSeGYY2kHIc1gtDGJ4bqNdbyVTT0aEe3bavA2tTUX2JIhHy1eIL6EMr2gw3cV";
 
-const nhost = new NhostClient({
-  backendUrl: "https://jeosnbwsaxhkwehlcxqj.nhost.run",
-});
+const runApiOperation = async (
+  operation: string,
+  variables: Record<string, any>,
+) => {
+  const response = await fetch(
+    "https://bgpopknowleadge.hasura.app/v1/graphql",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": ADMIN_SECRET,
+      },
+      body: JSON.stringify({
+        query: operation,
+        variables,
+      }),
+    },
+  );
+
+  const data = await response.json();
+  return data as { data: Record<string, any>; error?: any };
+};
 
 const cacheGet = async (key: string) => {
-  const response = await nhost.graphql.request(
+  const response = await runApiOperation(
     `query getCache($key: String!) {
     hit: cache_by_pk(key: $key) {
       value
@@ -13,16 +33,19 @@ const cacheGet = async (key: string) => {
   }`,
     { key },
   );
+  console.log(response);
 
-  const cachedValue = (response.data as { hit?: { value: Record<string, any> } }).hit?.value;
+  const cachedValue = (
+    response.data as { hit?: { value: Record<string, any> } }
+  ).hit?.value;
 
-  if (cachedValue) console.log(`[Cache] cache hit for key: "${key}"`)
+  if (cachedValue) console.log(`[Cache] cache hit for key: "${key}"`);
 
   return cachedValue ? cachedValue : null;
 };
 
 const cacheSet = async <T>(key: string, value: T) => {
-  const response = await nhost.graphql.request(
+  const response = await runApiOperation(
     `mutation upsertCache($key: String!, $value: json) {
       insert_cache_one(
         object: {key: $key, value: $value}
@@ -38,7 +61,7 @@ const cacheSet = async <T>(key: string, value: T) => {
   );
 
   if (response.error) {
-    throw Error(JSON.stringify(response.error))
+    throw Error(JSON.stringify(response.error));
   }
 
   return value;
